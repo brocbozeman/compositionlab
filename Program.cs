71d262b3afcf9b671292e25492b46ef608d2bd29
brocbozeman <78227881+brocbozeman@users.noreply.github.com>
2021-11-22 13:39:54 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace composition_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Author myAuthor = new Author("Broc Bozeman" , 'M' , "brocbozeman@gmail.com");
+            Console.WriteLine(myAuthor.ToString());
+
+            Book myBook = new Book("12345" , "How to Finish a Lab" , myAuthor );
+            Console.WriteLine(myBook.ToString());
+
+            Book otherBook = new Book("22222" , "How to Write a Lab", new Author("Jamie", 'F', "Jamie@gmail.com"));
+            Console.WriteLine(otherBook.ToString());
+
+            otherBook.GetAuthor().SetEmail("jamie@crimson.ua.edu");
+            otherBook.GetEmail();
+            Console.WriteLine(otherBook.ToString());
+        }
+    }
+}
